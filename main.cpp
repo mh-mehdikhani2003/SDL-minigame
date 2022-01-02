49146817ded11a9f0e58227d76c0d7d234dea141
GHOST-mHBr <ghostmhbr@gmail.com>
2022-01-02 10:46:08 +0330

    gitignore added and working on low speed of events

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 
 #include <string>
 #include <fstream>
+#include <ctime>
 
 #ifdef __unix__
 #ifndef SCORE_FILE_ADDR
@@ -28,6 +29,15 @@
 #endif
 #endif
 
+#ifndef SCORE_FILE_ADDR
+#define SCORE_FILE_ADDR ".\scores"
+#endif
+
+#ifndef __GFX__
+#include <SDL2/SDL_gfx>
+#endif
+
+#define CHAR_RAW_ROOT "./raw/Char/"
 #define FONT_ADDR "./arial.ttf"
 #define UME_SCORE_FONT_SIZE 35
 #define UME_BTNS_FONT_SIZE 16
@@ -36,9 +46,41 @@
 #define WIDNOW_HEIGHT 720
 
 #define DELAY 50
+#define GRAVITY 10
+
+#define WIDTH 1200
+#define HEIGHT 800
 
 using namespace std;
 
+//-----===== OK! Tested ====-----
+bool draw_image_on_point(SDL_Renderer *renderer, SDL_Point buttom_center_point, Uint16 height, const char *image_address, SDL_Rect *srcrect = NULL, SDL_Rect *dstrect = NULL)
+{
+    SDL_Surface *surf = IMG_Load(image_address);
+    if (!surf)
+    {
+        return false;
+    }
+
+    float scale = (float)height / (float)surf->h;
+    SDL_Rect rect = {buttom_center_point.x - (int)(surf->w / 2.0 * scale), buttom_center_point.y - (int)(surf->h * scale), (int)(surf->w * scale), (int)(surf->h * scale)};
+
+    SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surf);
+    if (!texture)
+    {
+        SDL_FreeSurface(surf);
+        return false;
+    }
+    SDL_Texture *former_texture = SDL_GetRenderTarget(renderer);
+    // SDL_SetRenderTarget(renderer, NULL);
+    SDL_RenderCopy(renderer, texture, srcrect, (dstrect == NULL ? &rect : dstrect));
+
+    SDL_FreeSurface(surf);
+    SDL_DestroyTexture(texture);
+    SDL_SetRenderTarget(renderer, former_texture);
+    former_texture = NULL;
+    return true;
+}
 //----==== OK! Tested ====-----
 SDL_Rect render_text_center(SDL_Renderer *renderer, const char *text, SDL_Point *center_point, TTF_Font *font = NULL, SDL_Color color = {0, 0, 0, 255})
 {
@@ -60,12 +102,12 @@ SDL_Rect render_text_center(SDL_Renderer *renderer, const char *text, SDL_Point 
     return result;
 }
 
-SDL_Rect render_text_right(SDL_Renderer *renderer, const char *text, SDL_Point *top_right_corner, TTF_Font *font = TTF_OpenFont(FONT_ADDR, 20), SDL_Color color = {0, 0, 0, 255})
+SDL_Rect render_text_right(SDL_Renderer *renderer, const char *text, SDL_Point *right_center_point, TTF_Font *font = TTF_OpenFont(FONT_ADDR, 20), SDL_Color color = {0, 0, 0, 255})
 {
     SDL_Texture *pervious_target = SDL_GetRenderTarget(renderer);
     SDL_Surface *surf = TTF_RenderText_Blended(font, text, color);
     SDL_Texture *txt = SDL_CreateTextureFromSurface(renderer, surf);
-    SDL_Rect result = {top_right_corner->x - surf->w, top_right_corner->y, surf->w, surf->h};
+    SDL_Rect result = {right_center_point->x - surf->w, right_center_point->y - surf->h / 2, surf->w, surf->h};
 
     SDL_SetRenderTarget(renderer, NULL);
     SDL_RenderCopy(renderer, txt, NULL, &result);
@@ -77,12 +119,12 @@ SDL_Rect render_text_right(SDL_Renderer *renderer, const char *text, SDL_Point *
     return result;
 }
 
-SDL_Rect render_text_left(SDL_Renderer *renderer, const char *text, SDL_Point *top_left_corner, TTF_Font *font = TTF_OpenFont(FONT_ADDR, 20), SDL_Color color = {0, 0, 0, 255})
+SDL_Rect render_text_left(SDL_Renderer *renderer, const char *text, SDL_Point *left_center_point, TTF_Font *font = TTF_OpenFont(FONT_ADDR, 20), SDL_Color color = {0, 0, 0, 255})
 {
     SDL_Texture *pervious_target = SDL_GetRenderTarget(renderer);
     SDL_Surface *surf = TTF_RenderText_Blended(font, text, color);
     SDL_Texture *txt = SDL_CreateTextureFromSurface(renderer, surf);
-    SDL_Rect result = {top_left_corner->x, top_left_corner->y, surf->w, surf->h};
+    SDL_Rect result = {left_center_point->x, left_center_point->y - surf->h / 2, surf->w, surf->h};
 
     SDL_SetRenderTarget(renderer, NULL);
     SDL_RenderCopy(renderer, txt, NULL, &result);
@@ -104,9 +146,278 @@ typedef enum States
 } States;
 States Game_State = STATE_END_MENU;
 
+typedef enum Char_modes
+{
+    NORMAL,
+    CONFUSED,
+    TRIPLE,
+    RUNNING,
+    JUMPING
+} Char_modes;
+typedef struct Character
+{
+private:
+    Char_modes mode = NORMAL;
+    int char_num = 1;
+    int current_pic_number = 3;
+    string img_addr = string(CHAR_RAW_ROOT) + string("1.png");
+    const static int char_pics_count = 4;
+
+    const int x_speed = 30;
+    const int y_speed = 50;
+    int dx = 0, dy = 0, dvy = -10;
+
+    SDL_Event *event = NULL;
+
+    SDL_Rect bounds{0, 0, 0, 0};
+    SDL_Texture *back_texture = NULL;
+
+    SDL_Rect pics_bounds[char_pics_count] = {0, 0, 0, 0};
+    SDL_Texture *pics_textures[char_pics_count] = {NULL, NULL, NULL, NULL};
+
+    void fill_pics_textures(SDL_Renderer *renderer)
+    {
+        for (int i = 0; i < char_pics_count; i++)
+        {
+            SDL_Surface *img = IMG_Load(create_address(char_num, i).c_str());
+            pics_textures[i] = SDL_CreateTextureFromSurface(renderer, img);
+            pics_bounds[i] = img->clip_rect;
+            SDL_FreeSurface(img);
+        }
+    }
+
+    void destroy_pics_textures()
+    {
+        for (int i = 0; i < char_pics_count; i++)
+        {
+            SDL_DestroyTexture(pics_textures[i]);
+            pics_textures[i] = NULL;
+        }
+    }
+
+    string create_address()
+    {
+        string result = "";
+        result += "./raw/Char/";
+        result += to_string(char_num);
+        if (current_pic_number != 0)
+        {
+            result += "(";
+            result += to_string(current_pic_number);
+            result += ")";
+        }
+        result += ".png";
+        return result;
+    }
+    string create_address(int char_number, int pic_number)
+    {
+        string result = "";
+        result += "./raw/Char/";
+        result += to_string(char_number);
+        if (pic_number != 0)
+        {
+            result += "(";
+            result += to_string(pic_number);
+            result += ")";
+        }
+        result += ".png";
+        return result;
+    }
+    void set_mode()
+    {
+        switch (event->type)
+        {
+        case SDL_KEYDOWN:
+        {
+            switch (event->key.keysym.sym)
+            {
+            case SDLK_UP:
+                mode = JUMPING;
+                dy = y_speed;
+                break;
+            case SDLK_LEFT:
+                mode = RUNNING;
+                dx = -x_speed;
+                break;
+            case SDLK_RIGHT:
+                mode = RUNNING;
+                dx = x_speed;
+                break;
+            }
+        }
+        break;
+
+        case SDL_KEYUP:
+        {
+            switch (event->key.keysym.sym)
+            {
+            case SDLK_UP:
+                break;
+
+            case SDLK_LEFT:
+                mode = NORMAL;
+                dx = 0;
+                SDL_PumpEvents();
+                SDL_FlushEvents(SDL_KEYDOWN, SDL_KEYUP);
+                break;
+            case SDLK_RIGHT:
+                mode = NORMAL;
+                dx = 0;
+                SDL_PumpEvents();
+                SDL_FlushEvents(SDL_KEYDOWN, SDL_KEYUP);
+                break;
+            }
+        }
+        break;
+        }
+    }
+
+public:
+    Character(SDL_Renderer *renderer, SDL_Event *event, SDL_Rect bounds, int char_num)
+    {
+        this->char_num = char_num;
+        this->bounds = bounds;
+        this->event = event;
+        fill_pics_textures(renderer);
+    }
+    Character(Character &character) = delete;
+    void set_mode(Char_modes mode)
+    {
+        this->mode = mode;
+    }
+    Char_modes get_mode()
+    {
+        return mode;
+    }
+    void set_texture(SDL_Texture *new_texture)
+    {
+        back_texture = new_texture;
+    }
+    SDL_Texture *get_texture()
+    {
+        return back_texture;
+    }
+    void render(SDL_Renderer *renderer, SDL_Rect *srcrect)
+    {
+        SDL_Texture *former_texture = SDL_GetRenderTarget(renderer);
+        set_mode();
+        switch (mode)
+        {
+        case NORMAL:
+        {
+            dx = 0;
+            dy = 0;
+            dvy = 0;
+            current_pic_number = 3;
+            break;
+        }
+        case CONFUSED:
+        {
+            SDL_SetRenderTarget(renderer, NULL);
+            SDL_RenderCopy(renderer, back_texture, srcrect, &bounds);
+            break;
+        }
+        case JUMPING:
+        {
+
+            break;
+        }
+        case TRIPLE:
+        {
+            SDL_SetRenderTarget(renderer, NULL);
+            SDL_RenderCopy(renderer, back_texture, srcrect, &bounds);
+            break;
+        }
+        case RUNNING:
+        {
+            current_pic_number += 1;
+            if (current_pic_number > 3)
+            {
+                current_pic_number = 0;
+            }
+            bounds.x += dx;
+            break;
+        }
+        }
+        float scale = (float)bounds.h / (float)pics_bounds[current_pic_number].h;
+        SDL_Rect rect = {bounds.x,
+                         bounds.y,
+                         (int)(pics_bounds[current_pic_number].w * scale),
+                         (int)(pics_bounds[current_pic_number].h * scale)};
+
+        SDL_SetRenderTarget(renderer, NULL);
+        SDL_RenderCopy(renderer, pics_textures[current_pic_number], NULL, &rect);
+        SDL_SetRenderTarget(renderer, former_texture);
+    }
+    ~Character()
+    {
+        destroy_pics_textures();
+    }
+} Character;
+
+typedef struct Timer
+{
+private:
+    Uint64 start_time = 0;
+    Uint64 total_time = 0;
+    Uint16 alarm = 0;
+    bool is_started = false;
+    bool is_on = false;
+    Timer(Timer &timer) = delete;
+
+public:
+    Timer()
+    {
+        start_time = time(nullptr);
+        is_on = true;
+        is_started = true;
+    }
+    void play()
+    {
+        is_on = true;
+    }
+    void pause()
+    {
+        if (is_on)
+        {
+            total_time = time(nullptr) - start_time;
+        }
+        start_time = time(nullptr) - total_time;
+        is_on = false;
+    }
+    Uint64 get_time()
+    {
+        if (is_on)
+        {
+            return time(nullptr) - start_time;
+        }
+        return total_time;
+    }
+    void set_alarm(Uint16 time_in_seconds)
+    {
+        alarm = time_in_seconds;
+    }
+
+    bool get_status()
+    {
+        return is_on;
+    }
+
+    void clear()
+    {
+        total_time = 0;
+        start_time = time(nullptr);
+    }
+
+    bool check_alarm()
+    {
+        return get_time() > alarm;
+    }
+} Timer;
+
 typedef struct Text
 {
-    std::string text = "";
+    std::string text = " ";
     SDL_Color color = {0, 0, 0, 255};
     int ptsize = 20;
     std::string font_addr = FONT_ADDR;
@@ -130,12 +441,13 @@ private:
     SDL_Rect bounds;
     Text text;
     SDL_Texture *back_texture = NULL;
+    SDL_Color back_color{0, 0, 0, 0};
 
-    void init_text(){
+    void init_text()
+    {
         text.text = " ";
     }
 
-    
 public:
     Button(SDL_Renderer *p_renderer, SDL_Surface *back, SDL_Rect bounds)
     {
@@ -229,20 +541,36 @@ public:
     }
     void render(SDL_Renderer *renderer, SDL_Rect *srcrect = NULL)
     {
-        int x = 0 , y = 0;
-        SDL_GetMouseState(&x , &y);
+        int x = 0, y = 0;
+        SDL_GetMouseState(&x, &y);
         SDL_Texture *former_texture = SDL_GetRenderTarget(renderer);
-        SDL_SetRenderTarget(renderer , NULL);
-        SDL_RenderCopy(renderer , back_texture , srcrect , &bounds);
-        
-        if(SDL_PointInRect(new SDL_Point{x,y} , &bounds)){
-            SDL_SetRenderDrawColor(renderer , 250 , 100 , 0 , 255);
-            SDL_RenderDrawRect(renderer , &bounds);
+        SDL_SetRenderTarget(renderer, NULL);
+        SDL_RenderCopy(renderer, back_texture, srcrect, &bounds);
+
+        if (SDL_PointInRect(new SDL_Point{x, y}, &bounds))
+        {
+            SDL_SetRenderDrawColor(renderer, 250, 100, 0, 255);
+            SDL_RenderDrawRect(renderer, &bounds);
         }
-        
-        render_text_center(renderer , text.text.c_str() , new SDL_Point{bounds.w/2 + bounds.x , bounds.h/2+bounds.y} , text.font , text.color);
-        SDL_SetRenderTarget(renderer , former_texture);
+
+        render_text_center(renderer, text.text.c_str(), new SDL_Point{bounds.w / 2 + bounds.x, bounds.h / 2 + bounds.y}, text.font, text.color);
+        SDL_SetRenderTarget(renderer, former_texture);
         former_texture = NULL;
+    }
+    SDL_Color get_color()
+    {
+        return back_color;
+    }
+    void set_back_color(SDL_Renderer *renderer, SDL_Color color)
+    {
+        back_color = color;
+        SDL_Texture *former = SDL_GetRenderTarget(renderer);
+        SDL_SetRenderTarget(renderer, back_texture);
+        SDL_SetRenderDrawColor(renderer, back_color.r, back_color.g, back_color.b, back_color.a);
+        SDL_RenderClear(renderer);
+        SDL_SetRenderTarget(renderer, NULL);
+        SDL_RenderCopy(renderer, back_texture, NULL, &bounds);
+        SDL_SetRenderTarget(renderer, former);
     }
     ~Button()
     {
@@ -252,7 +580,7 @@ public:
 
 typedef struct Button Button;
 
-int end_mnu_btns[END_MENU_BTN_CNT][4];
+// int end_mnu_btns[END_MENU_BTN_CNT][4];
 
 bool read_best_score(long &score)
 {
@@ -297,34 +625,7 @@ bool set_best_score(const long score)
     return false;
 }
 
-//-----===== OK! Tested ====-----
-bool draw_image(SDL_Renderer *renderer, SDL_Point top_left_corner, const char *image_address , SDL_Rect * srcrect = NULL , SDL_Rect * dstrect = NULL)
-{
-    SDL_Surface *surf = IMG_Load(image_address);
-    if (!surf)
-    {
-        return false;
-    }
-    SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surf);
-    if (!texture)
-    {
-        SDL_FreeSurface(surf);
-        return false;
-    }
-    SDL_Texture *former_texture = SDL_GetRenderTarget(renderer);
-
-    SDL_Rect rect = {top_left_corner.x, top_left_corner.y, surf->w, surf->h};
-    SDL_SetRenderTarget(renderer, NULL);
-    SDL_RenderCopy(renderer, texture, srcrect, (dstrect == NULL?&rect : dstrect));
-
-    SDL_FreeSurface(surf);
-    SDL_DestroyTexture(texture);
-    SDL_SetRenderTarget(renderer, former_texture);
-    former_texture = NULL;
-    return true;
-}
-
-int end_menu(SDL_Renderer *renderer, SDL_Texture *back_texture, SDL_Rect *bounds, TTF_Font *text_font)
+/*int end_menu(SDL_Renderer *renderer, SDL_Texture *back_texture, SDL_Rect *bounds, TTF_Font *text_font)
 {
 
     SDL_SetRenderTarget(renderer, NULL);
@@ -402,44 +703,49 @@ int end_menu(SDL_Renderer *renderer, SDL_Texture *back_texture, SDL_Rect *bounds
     SDL_DestroyTexture(texture_score);
 
     return 1;
-}
-
-
-
-
+}*/
 
 int main(int argc, char *argv[])
 {
-    // Initialization of SDL window
-    Uint32 SDL_flags = SDL_INIT_VIDEO | SDL_INIT_EVENTS;
-    Uint32 WND_flags = SDL_WINDOW_HIDDEN;
-
     SDL_Window *m_window = NULL;
     SDL_Renderer *m_renderer = NULL;
-
-    if (SDL_Init(SDL_flags) < 0 || TTF_Init() < 0 || IMG_Init(IMG_INIT_JPG | IMG_INIT_PNG | IMG_INIT_TIF | IMG_INIT_WEBP) == 0)
+    // Initialization of SDL window
     {
-        printf("Error with SDL_Init: %s", SDL_GetError());
-        return -1;
+        Uint32 SDL_flags = SDL_INIT_VIDEO | SDL_INIT_EVENTS;
+        Uint32 WND_flags = SDL_WINDOW_HIDDEN;
+
+        if (SDL_Init(SDL_flags) < 0 || TTF_Init() < 0 || IMG_Init(IMG_INIT_JPG | IMG_INIT_PNG | IMG_INIT_TIF | IMG_INIT_WEBP) == 0)
+        {
+            printf("Error with SDL_Init: %s", SDL_GetError());
+            return -1;
+        }
+
+        SDL_CreateWindowAndRenderer(WINDOW_WIDTH, WIDNOW_HEIGHT, WND_flags, &m_window, &m_renderer);
+        if (!m_renderer || !m_window)
+        {
+            printf("Error with renderer or window: %s", SDL_GetError());
+            return -1;
+        }
+
+        SDL_SetRenderDrawColor(m_renderer, 0, 0, 0, 255);
+        SDL_ShowWindow(m_window);
+        SDL_RenderClear(m_renderer);
+        SDL_RenderPresent(m_renderer);
     }
 
-    SDL_CreateWindowAndRenderer(WINDOW_WIDTH, WIDNOW_HEIGHT, WND_flags, &m_window, &m_renderer);
-    if (!m_renderer || !m_window)
-    {
-        printf("Error with renderer or window: %s", SDL_GetError());
-        return -1;
-    }
-
-    SDL_SetRenderDrawColor(m_renderer, 0, 0, 0, 255);
-    SDL_ShowWindow(m_window);
-    SDL_RenderClear(m_renderer);
-    SDL_RenderPresent(m_renderer);
-
+    //-----====-Create main elements-====-----
     SDL_Event *e = new SDL_Event;
-    TTF_Font *font = TTF_OpenFont(FONT_ADDR, 30);
+    Text text;
+    Timer timer;
 
+    Character l_char(m_renderer, e, {100, 400, 100, 300}, 1);
+    Character r_char(m_renderer, e, {500, 400, 100, 300}, 2);
+
+    //-----====-Main game loop start-====-----
     while (Game_State != STATE_QUIT)
     {
+        SDL_PollEvent(e);
+
         SDL_SetRenderTarget(m_renderer, NULL);
         SDL_SetRenderDrawColor(m_renderer, 30, 40, 50, 255);
         SDL_RenderClear(m_renderer);
@@ -454,27 +760,25 @@ int main(int argc, char *argv[])
             break;
         case STATE_END_MENU:
         {
-            draw_image(m_renderer , {0 , 0} , "./1.png" , NULL);
+            r_char.render(m_renderer, NULL);
+            l_char.render(m_renderer, NULL);
             break;
         }
         case STATE_QUIT:
             break;
         }
-
-        while (SDL_PollEvent(e))
+        //-----====-Checking for quit and other events-====-----
+        switch (e->type)
         {
-            switch (e->type)
+        case SDL_KEYDOWN:
+            if (e->key.keysym.sym == 'q')
             {
-            case SDL_KEYDOWN:
-                if (e->key.keysym.sym == 'q')
-                {
-                    Game_State = STATE_QUIT;
-                }
-                break;
-            case SDL_QUIT:
                 Game_State = STATE_QUIT;
-                break;
             }
+            break;
+        case SDL_QUIT:
+            Game_State = STATE_QUIT;
+            break;
         }
 
         SDL_RenderPresent(m_renderer);
